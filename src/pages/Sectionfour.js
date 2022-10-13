4a34926c39c1129700a4ba2ac0568fa8aaa5ca0f
Timguse <tguse@casema.nl>
2022-10-13 16:40:49 +0200

    foto update

--- src/pages/Sectionfour.js
+++ src/pages/Sectionfour.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles/styleSectionfour.css";
-import tim from "../images/profile-picture.png";
+import tim from "../images/tims.png";
+import max from "../images/max.png";
 import IconL from "../images/linkedin.png";
 import IconF from "../images/facebook.png";
 import IconI from "../images/insta.png";
@@ -16,7 +17,7 @@ function Sectionfour(){
             <div className="sectionfour-blok">
                 <div className="profielafbeeldingvlak">
                     <div className="tim" data-aos="fade-left" data-aos-duration="10000">
-                        <img className="profielfoto" src={tim}></img>
+                        <img className="profielfoto" src={tim} alt=" "></img>
                        <br/><br/>
                         <h2>Tim Güse</h2>
                         <br/>
@@ -25,24 +26,24 @@ function Sectionfour(){
                         <br/>
                         <div className="socialmedia-locatie">
                             <div className="socialmedia-icons">
-                                <a href="https://www.linkedin.com/" target="_blank">
-                                    <img className="icon-l-f-i" src={IconL}></img>
+                                <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+                                    <img className="icon-l-f-i" src={IconL} alt=" "></img>
                                 </a>
                             </div>
                             <div className="socialmedia-icons">
-                                <a href="https://www.facebook.com/" target="_blank">
-                                    <img className="icon-l-f-i" src={IconF}></img>
+                                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+                                    <img className="icon-l-f-i" src={IconF} alt=" "></img>
                                 </a>
                             </div>
                             <div className="socialmedia-icons">
-                            <a href="https://www.instagram.com/" target="_blank">
-                                <img className="icon-l-f-i" src={IconI}></img>
+                            <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+                                <img className="icon-l-f-i" src={IconI} alt=" "></img>
                             </a>
                             </div>
                         </div>
                     </div>
                     <div className="max" data-aos="fade-left" data-aos-duration="10000">
-                        <img className="profielfoto1" src={tim}></img>
+                        <img className="profielfoto1" src={max} alt=" "></img>
                         <br/><br/>
                         <h2>Max Jansen</h2>
                         <br/>
@@ -51,18 +52,18 @@ function Sectionfour(){
                         <br/>
                         <div className="socialmedia-locatie">
                             <div className="socialmedia-icons">
-                                <a href="https://www.linkedin.com/" target="_blank">
-                                    <img className="icon-l-f-i" src={IconL}></img>
+                                <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+                                    <img className="icon-l-f-i" src={IconL} alt=" "></img>
                                 </a>
                             </div>
                             <div className="socialmedia-icons">
-                                <a href="https://www.facebook.com/" target="_blank">
-                                    <img className="icon-l-f-i" src={IconF}></img>
+                                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+                                    <img className="icon-l-f-i" src={IconF} alt=" "></img>
                                 </a>
                             </div>
                             <div className="socialmedia-icons">
-                            <a href="https://www.instagram.com/" target="_blank">
-                                <img className="icon-l-f-i" src={IconI}></img>
+                            <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+                                <img className="icon-l-f-i" src={IconI} alt=" "></img>
                             </a>
                             </div>
                         </div>
